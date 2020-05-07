1e26c753f00d983053b41772f63a010bc5c50b78
natrng <naadiakisa@gmail.com>
2020-05-06 20:52:11 -0700

    identity

--- Persistence/Seed.cs
+++ Persistence/Seed.cs
@@ -2,13 +2,38 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using Domain;
+using Microsoft.AspNetCore.Identity;
 
 namespace Persistence
 {
     public class Seed
     {
-        public static void SeedData(DataContext context)
+        public static void SeedData(DataContext context, UserManager<AppUser> userManager)
         {
+            if (!userManager.Users.Any())
+            {
+                var users = new List<AppUser>{
+                    new AppUser{
+                        DisplayName = "Bob",
+                        UserName = "bob",
+                        Email = "bob@test.com"
+                    },
+                    new AppUser{
+                        DisplayName = "Tom",
+                        UserName = "tom",
+                        Email = "tom@test.com"
+                    },                    
+                    new AppUser{
+                        DisplayName = "Jane",
+                        UserName = "jane",
+                        Email = "jane@test.com"
+                    }
+                };
+
+                foreach(var user in users){
+                    userManager.CreateAsync(user);
+                }
+            }
             if (!context.Activities.Any())
             {
                 var activities = new List<Activity>
